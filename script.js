53bb13c8c6d9d44b0ec6a40c3c85cbd5c305ca25
Solange Demey <ssdemey@gmail.com>
2023-03-12 19:25:19 -0300

    Subiendo clase 3

---
+++ script.js
@@ -0,0 +1,94 @@
+/*console.log ("Inicia...")
+const weather = "soleado";
+
+if (weather=== "lluvioso"){
+    console.log ("llevá paraguas")
+
+
+} else {
+    console.log("el tiempo esta", weather);
+}
+
+console.log ("Fin del programa...");
+
+//escribir un algoritmo que determine el mayor de dos números
+//let num_1 =100;
+//let num_2 =1200;
+//let result = "";
+//if (num_1> num_2){ result = "El número  " +  num_1 + " es mayor que " + num_2;}
+// else if ( num_1<num_2){    result = "El número " + num_2 +  " es mayor que " + num_1}
+//else{result = "El número " + num_2 + " es igual que " + num_1;}
+//console.log (result) 
+//const today = ""
+console.log ( new Date() .getFullYear() );
+//fido.moverLaCola
+//fido.comerComida asi le doy ordenes a los objetos
+
+
+//escribir algoritmo que determine el dia de la semana
+//const today = new Date ().getDay;
+//let day = "";
+
+
+
+
+/*if(today === 0){
+    day = "domingo"
+} else if (today === 1) {
+    day = "lunes"
+}
+else if (today === 2) {
+    day = "martes"
+}else if (today === 3) {
+    day = "miercoles"
+}else if (today === 4) {
+    day = "jueves"
+}else if (today === 5) {
+    day = "viernes"
+}else{
+    day = "sabado";
+
+}
+console.log ("Hoy es ", day);
+
+switch (today){
+    case 0 : {
+        day = "domingo"
+       break;
+    }
+    case 1 : {
+        day = "lunes"
+        break;
+    }
+    case 2 : {
+        day = "martes"
+        break;
+    }
+    case 3 : {
+        day = "miercoles"
+       break;
+    }
+    case 4 : {
+        day = "jueves"
+       break;
+    }
+    case 5 : {
+        day = "viernes"
+        break;
+    }
+    case 6 : {
+        day = "sabado"
+        break;
+    }
+    default:
+        day = "se rompió la matrix changos"
+        break;
+}
+console.log("Hoy es" , day + "con switch");*/
+
+let counter = 0
+while (counter<5){
+    console.count("hola") ;
+    counter = counter +1  //counter
+
+}
